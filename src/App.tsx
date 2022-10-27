3b16ce68c459f8fcb33896b9f8273750ee672804
VigM <vmikulis@figure.com>
2022-10-27 12:55:49 -0600

    demo complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useWalletConnect, QRCodeModal } from "@provenanceio/walletconnect-js";
+import React from "react";
+import "./App.css";
 
 function App() {
+  const { walletConnectService, walletConnectState } = useWalletConnect();
+  const {
+    connected,
+    address,
+    publicKey,
+    signedJWT,
+    walletInfo = {},
+  } = walletConnectState;
+  const { name } = walletInfo;
+
   return (
-    <div className="App">
+    <section className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {connected ? (
+          <div>
+            <p>
+              You've Connected with "<code>{name}</code>" wallet!
+            </p>
+            <p>Address:</p>
+            <code>{address}</code>
+            <p>PublicKey:</p>
+            <code>{publicKey}</code>
+            <p>Signed JWT:</p>
+            <code>{signedJWT}</code>
+            <div>
+              <button onClick={() => walletConnectService.disconnect()}>
+                Disconnect
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>Click the connect button to link your wallet to this app</p>
+            <button onClick={() => walletConnectService.connect()}>
+              Connect
+            </button>
+          </div>
+        )}
+        <QRCodeModal walletConnectService={walletConnectService} />
       </header>
-    </div>
+    </section>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { WalletConnectContextProvider } from "@provenanceio/walletconnect-js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WalletConnectContextProvider>
+      <App />
+    </WalletConnectContextProvider>
   </React.StrictMode>
 );
 
