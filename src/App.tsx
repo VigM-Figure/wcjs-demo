ae22cf6cb5e3eccb8882afa11de083e087c48e78
VigM <vmikulis@figure.com>
2022-11-03 14:20:16 -0600

    remove extra methods tests

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,6 @@ function App() {
   } = walletConnectState;
   const { name } = walletInfo;
 
-  const handleErrorTest = () => {
-    const messageArray = [
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEoQBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLC9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ1dyaXRlU2NvcGVSZXF1ZXN0",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEoUBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLS9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0RlbGV0ZVNjb3BlUmVxdWVzdA==",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEowBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaNC9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0FkZFNjb3BlRGF0YUFjY2Vzc1JlcXVlc3Q=",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEo8BCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaNy9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0RlbGV0ZVNjb3BlRGF0YUFjY2Vzc1JlcXVlc3Q=",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEocBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLy9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0FkZFNjb3BlT3duZXJSZXF1ZXN0",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEooBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaMi9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0RlbGV0ZVNjb3BlT3duZXJSZXF1ZXN0",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEoYBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLi9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ1dyaXRlU2Vzc2lvblJlcXVlc3Q=",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEoUBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLS9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ1dyaXRlUmVjb3JkUmVxdWVzdA==",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEoYBCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaLi9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ0RlbGV0ZVJlY29yZFJlcXVlc3Q=",
-      "Ch8vY29zbW9zLmF1dGh6LnYxYmV0YTEuTXNnUmV2b2tlEpABCil0cDFuZGM2djVjejI2czRhbHM0ejV6MmQ5MGY4dWx5dnNnYzB1ZHhycRIpdHAxbTh1dWc1bmQwd2RsNDV3OWdrdGUzMG5nNGU1eXk3bW1jd2x5dWQaOC9wcm92ZW5hbmNlLm1ldGFkYXRhLnYxLk1zZ1A4ZU1lbW9yaWFsaXplQ29udHJhY3RSZXF1ZXN0",
-    ];
-    walletConnectService.sendMessage({ message: messageArray });
-  };
-  const handleCoinSend = () => {
-    const message =
-      "ChwvY29zbW9zLmJhbmsudjFiZXRhMS5Nc2dTZW5kEmwKKXRwMWtuc3hmbm4wbHE0OG1tbmtmbmtndGtrOHFueHhkdTB5MnRrbGtoEil0cDFrbnN4Zm5uMGxxNDhtbW5rZm5rZ3Rrazhxbnh4ZHUweTJ0a2xraBoUCgVuaGFzaBILMTAwMDAwMDAwMDA=";
-    walletConnectService.sendMessage({ message });
-  };
-
   return (
     <section className="App">
       <header className="App-header">
@@ -52,10 +31,6 @@ function App() {
               <button onClick={() => walletConnectService.disconnect()}>
                 Disconnect
               </button>
-            </div>
-            <div>
-              <button onClick={handleErrorTest}>Test Error</button>
-              <button onClick={handleCoinSend}>Test Coin Send</button>
             </div>
           </div>
         ) : (
